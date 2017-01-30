4aa968fbdd0199a30c920678d2c78e4eb769d78f
Patrickyp <Patrickyp@users.noreply.github.com>
2017-01-30 13:50:28 -1000

    Update main.py
    Changed from 3 cameras to 2.  Changed to work with DC Motor sketch.

--- main.py
+++ main.py
@@ -18,22 +18,23 @@ import datetime
 from FPS import FPS #See FPS.py.
 from WebcamVideoStream import WebcamVideoStream #See WebcamVideoStream.py
 import time
+from docutils.utils.math.math2html import Position
 
 
-ser = serial.Serial('/dev/ttyACM1', 115200) # Start serial communication with Arduino, change baud rate and port here.
-stepValues = [250,800,1200] # Step increments for move command on GUI
-step = stepValues[0]*2 # Default stepValue
+ser = serial.Serial('/dev/ttyACM0', 115200) # Start serial communication with Arduino, change baud rate and port here.
+stepValues = [30,60,120] # Step increments for move command on GUI
+step = stepValues[1] # Default stepValue
 running = False # This value is set to False when user clicks Stop, will quit Run loop
 
 # Set webcam index here
 vs = WebcamVideoStream(src=0).start()
 vs1 = WebcamVideoStream(src=1).start()
-vs2 = WebcamVideoStream(src=2).start()
+#vs2 = WebcamVideoStream(src=2).start()
 
 # Initialize video writer objects.
 out = None
 out1 = None
-out2 = None
+#out2 = None
 
 # Keep track of which camera is currently being displayed.
 # This variable is updated whenever a new camera is selected via radio button.
@@ -57,9 +58,9 @@ class PanTiltApp(QtGui.QDialog, ui.Ui_Dialog):
         # Define the methods called when a button is pressed
         self.Run.clicked.connect(self.run_gui)
         self.Stop.clicked.connect(self.stop_gui)
-        self.Step1.clicked.connect(self.step1)
-        self.Step2.clicked.connect(self.step2)
-        self.Step3.clicked.connect(self.step3)
+        self.StepSmall.clicked.connect(self.step1)
+        self.StepMedium.clicked.connect(self.step2)
+        self.StepLarge.clicked.connect(self.step3)
         self.MoveRight.clicked.connect(self.moveRight)
         self.MoveLeft.clicked.connect(self.moveLeft)
         self.MoveUp.clicked.connect(self.moveUp)
@@ -72,9 +73,9 @@ class PanTiltApp(QtGui.QDialog, ui.Ui_Dialog):
         self.SaveVideo.clicked.connect(self.saveVideo)
         
         # Set step button text
-        self.Step1.setText("Step " + str(stepValues[0]))
-        self.Step2.setText("Step " + str(stepValues[1]))
-        self.Step3.setText("Step " + str(stepValues[2]))
+        self.StepSmall.setText("Step " + str(stepValues[0]))
+        self.StepMedium.setText("Step " + str(stepValues[1]))
+        self.StepLarge.setText("Step " + str(stepValues[2]))
         
         # Limit valid inputs of Time, Az, Zen using validator 
         myRegex = QtCore.QRegExp("(\d+)(,\s*\d+)*") # Format of {int1,int2...intn} using regex
@@ -94,7 +95,7 @@ class PanTiltApp(QtGui.QDialog, ui.Ui_Dialog):
     def play(self):
         ###################################################################################################
         global recordStatus
-        global out,out1,out2
+        global out,out1#,out2
 
         # If saveVideo function set recordStatus to 1, start saving frames.
         if recordStatus == 1:
@@ -107,10 +108,10 @@ class PanTiltApp(QtGui.QDialog, ui.Ui_Dialog):
             else:
                 frame ,_ = vs.read()
                 frame1 ,_ = vs1.read()
-                frame2 ,_ = vs2.read()
+                #frame2 ,_ = vs2.read()
                 out.write(frame)
                 out1.write(frame1)
-                out2.write(frame2)
+                #out2.write(frame2)
         ####################################################################################################
         global currentCamera
         
@@ -128,7 +129,7 @@ class PanTiltApp(QtGui.QDialog, ui.Ui_Dialog):
     # When save video box is selected run this function.
     def saveVideo(self):
         global recordStatus
-        global out, out1, out2
+        global out, out1#, out2
         # Box was selected before selection, user wants to stop recording.
         if recordStatus == 1:
             print "finish recording!"
@@ -143,12 +144,12 @@ class PanTiltApp(QtGui.QDialog, ui.Ui_Dialog):
             # Create file name for the 3 videos
             name1 = "videos/" + getTime() + "cam1.avi"
             name2 = "videos/" + getTime() + "cam2.avi"
-            name3 = "videos/" + getTime() + "cam3.avi"
+            #name3 = "videos/" + getTime() + "cam3.avi"
             
             # initialize VideoWriter objects with file name.
             out = cv2.VideoWriter(name1,cv2.cv.CV_FOURCC('M','J','P','G'), 20.0, (1920,1080))
             out1 = cv2.VideoWriter(name2,cv2.cv.CV_FOURCC('M','J','P','G'), 20.0, (1920,1080))
-            out2 = cv2.VideoWriter(name3,cv2.cv.CV_FOURCC('M','J','P','G'), 20.0, (1920,1080))
+            #out2 = cv2.VideoWriter(name3,cv2.cv.CV_FOURCC('M','J','P','G'), 20.0, (1920,1080))
         print "saving video!"
     
     # Show camera1 feed on GUI
@@ -164,41 +165,41 @@ class PanTiltApp(QtGui.QDialog, ui.Ui_Dialog):
     # Show camera3 feed on GUI
     def showRightView(self):
         global currentCamera
-        currentCamera = vs2
+        #currentCamera = vs2
         
     # Take 3 images for each camera (9 total).
     def takeImageSet(self):
-        threeCameraTakeImage(self, 50, 80, False, 3)
+        takeImages(self, 50, 80, False, 3)
     
     # Take 1 image for each camera and label as CALIB (3 total).
     def takeCalibImages(self):
-        threeCameraTakeImage(self, 50, 80, True, 1)
+        takeImages(self, 50, 80, True, 1)
         
     def moveRight(self):
         updateStatus(self, "Moving Right...")
         # Motor # , direction, number of steps
-        output = str(99) + "," + str(1) + "," + str(step)
+        output = str(1) + "," + str(1) + "," + str(step)
         ser.write(output)
         __ = ser.readline()
         updateStatus(self, "Idle")
         
     def moveLeft(self):
         updateStatus(self, "Moving Left...")
-        output = str(99) + "," + str(0) + "," + str(step)
+        output = str(1) + "," + str(-1) + "," + str(step)
         ser.write(output)
         __ = ser.readline()
         updateStatus(self, "Idle")
         
     def moveUp(self):
         updateStatus(self, "Moving Up...")
-        output = str(98) + "," + str(1) + "," + str(step)
+        output = str(2) + "," + str(1) + "," + str(step)
         ser.write(output)
         __ = ser.readline()
         updateStatus(self, "Idle")
         
     def moveDown(self):
         updateStatus(self, "Moving Down...")
-        output = str(98) + "," + str(0) + "," + str(step)
+        output = str(2) + "," + str(-1) + "," + str(step)
         ser.write(output)
         __ = ser.readline()
         updateStatus(self, "Idle")
@@ -226,93 +227,98 @@ class PanTiltApp(QtGui.QDialog, ui.Ui_Dialog):
     def stop_gui(self):
         global running
         running = False
-        enableButtons(self, True)
+        buttonEnabledState(self, True)
+    
     # Start the run routine.  
     def run_gui(self):
-        
         global running
-        
+        updateStatus(self,"Starting run...")
         # Flush serial port buffer.
         ser.flushInput()
-        ser.flushOutput()
-        
-        
+        ser.flushOutput()        
+    
         # If not already running, start run, otherwise ignore button press
         if running == False:
             running = True
-            
+              
             # Disable buttons.
-            enableButtons(self, False)
+            buttonEnabledState(self, False)
             # Read values from 3 text inputs, viewList contains all possible combinations of (az,zen).  
             (sleepDuration, AzList, ZenList) = processInput(self.TimeInput.text(),self.AzInput.text(),self.ZenInput.text())
-            
-            # Find total number of steps for full rotation and 
-            stepsPerDegreePan, stepsPerDegreeTilt = findTotalSteps(self)
-            
-            # Keep track of Viewing Case.
-            viewIndex = 0 
-            
-            # For each iteration of this loop, find home, move through all views, take image, and sleep.  
-            # Check for stop button press periodically with app.processEvents
+              
+          
+            # For each iteration of this loop
             while True:
+                # Keep track of Viewing Case.
                 viewIndex = 0
-                # Tell pan motor(99) to find home.
-                findHome(self, 99)
                 
                 # Check for any GUI inputs from user i.e. stop pressed.
                 app.processEvents()
-                
                 # Check running variable for false value (set false when user clicks stop).  Break loop to stop routine if false.
                 if (running == False):
                     print "stopped!"
                     updateStatus(self,"Idle")
                     return
-                
-                # Outer loop goes through each Az angle, inner loop goes through each Zen angle.
+                  
+                # Outer loop goes through each Az (Pan) angle, inner loop goes through each Zen angle.
                 # I.e motor will go to each Az angle and go through all Zen angle while at each Az angle.
                 for i in range(0, len(AzList)):
-                    pointCamera(self, AzList[i], stepsPerDegreePan, 99)
                     # Update status message before each new Az
                     status = "Moving to Az " + str(AzList[i]) + " Zen " + str(ZenList[0]) 
                     updateStatus(self, status)
-                    print AzList[i]
-                    findHome(self,98)
+
+                    self.moveToPosition(1, AzList[i])
+                    #print AzList[i]
+
+                    
                     for j in range(0, len(ZenList)):
                         viewIndex+=1
                         # Don't need to update for the first zen since status already updated for every new Az above
                         if (j != 0):
                             status = "Moving to Az " + str(AzList[i]) + " Zen " + str(ZenList[j]) 
                             updateStatus(self, status)
-                        
-                        pointCamera(self, ZenList[j], stepsPerDegreeTilt, 98)
-                        print " ", ZenList[j]
+                          
+                        self.moveToPosition(2, ZenList[j])
+                        #print " ", ZenList[j]
                         # Update View/Az/Zen labels
                         self.ViewLabel.setText(str(viewIndex))
                         self.AzLabel.setText(str(AzList[i]))
                         self.ZenLabel.setText(str(ZenList[j]))
                         app.processEvents()
-                        
+                          
                         # Take 3 images from each camera.
-                        threeCameraTakeImage(self, str(AzList[i]), str(ZenList[j]), False, 3)
-                        
+                        takeImages(self, str(AzList[i]), str(ZenList[j]), False, 3)
+                          
                         #check stop button
                         app.processEvents()
                         if (running == False):
                             print "stopped!"
                             updateStatus(self,"Idle")
                             return
-                        
+                          
                         # Sleep for delay as entered from GUI
                         sleep(self,sleepDuration)
 
-
+                
+    def moveToPosition(self, motorNumber, degree):
+        command = str(motorNumber) + "," + str(0) + "," + str(degree)
+        ser.write(command)
+        result = ser.readline()
+        print result
+        
+    def checkStop(self):
+        app.processEvents()
+        if (running == False):
+            print "stopped!"
+            updateStatus(self,"Idle")
+            return
 # Enable or disable button during run because we don't want users to issue commands like Move Right while motor is doing its run routine.
 # buttonState should be either True (to enable) or False (to disable).
-def enableButtons(self,buttonState):
+def buttonEnabledState(self,buttonState):
     
-    self.Step1.setEnabled(buttonState)
-    self.Step2.setEnabled(buttonState)
-    self.Step3.setEnabled(buttonState)
+    self.StepSmall.setEnabled(buttonState)
+    self.StepMedium.setEnabled(buttonState)
+    self.StepLarge.setEnabled(buttonState)
     self.MoveRight.setEnabled(buttonState)
     self.MoveLeft.setEnabled(buttonState)
     self.MoveUp.setEnabled(buttonState)
@@ -323,79 +329,13 @@ def enableButtons(self,buttonState):
     self.ShowLStereo.setEnabled(buttonState)
     self.ShowRStereo.setEnabled(buttonState)
     self.SaveVideo.setEnabled(buttonState)
-    
+    self.Run.setEnabled(buttonState)
 # Flush serial port buffer.             
 def flushSerial():
     ser.flushInput()
     ser.flushOutput()
-    
-# Move the motor specified by motorID to position(in degree).
-# stepsPerDegree is how many steps the motor takes for 1 degree (~10).
-def pointCamera(self,position,stepsPerDegree,motorID):
-    # Calculate destination step location using position (degrees) and stepsPerDegree
-    destination = int(round(position * stepsPerDegree)) 
-    
-    # Move pan motor to az position.  Convert degrees to steps.  Round to the nearest whole number, then call int to remove trailing .0
-    command = str(motorID) + "," + str(-1) + "," + str(1) + "," + str(destination) #Motor #, command, direction, position
-    ser.write(command)
-    ser.readline()
 
-# Tell motor to find home.
-def findHome(self, motorNumber):
-    command = str(motorNumber) + "," + str(-1) + "," + str(0)
-    ser.write(command)
-    __ = ser.readline()
-     
-# Tell motor to find total steps in full rotation, update global totalStepsPan variable and return number of steps per degree
-def findTotalSteps(self):
     
-    updateStatus(self,"Calculating total steps.")
-
-
-    ####### Tell PAN motor to find total steps.  99 means az motor, -1 means command, 2 means find total steps. ########
-    command = str(99) + "," + str(-1) + "," + str(2)
-    ser.write(command)
-    
-    # NOTE: Need to read 3 values back, 2 for findHome and 1 for findTotalSteps.  This is needed otherwise program will skip to next part without
-    # waiting for motor operation to complete.
-    
-    # findHome
-    message = ser.readline()
-    print "[PAN]", message
-    updateStatus(self, "[PAN]" + message.strip())
-    
-    # findTotalSteps
-    message = ser.readline()
-    message = message.strip()
-    print "[PAN]",message
-    updateStatus(self, "[PAN]Total steps:" + message)
-    totalStepsPan = int(message)
-    stepsPerDegreePan = totalStepsPan / 360
-    
-    #findHome
-    message = ser.readline()
-    print "[PAN]",message
-    updateStatus(self, "[PAN]" + message.strip())
-    
-    
-    ####### Tell TILT motor to find total steps.  98 means tilt(zen) motor, -1 means command, 2 means find total steps. ########
-    command = str(98) + "," + str(-1) + "," + str(2)
-    ser.write(command)
-    
-    # findHome
-    message = ser.readline()
-    print "[TILT]",message
-    updateStatus(self, "[TILT]" + message.strip())
-    
-    # findTotalSteps
-    message = ser.readline()
-    msg = message.strip()
-    print "[TILT]",msg
-    updateStatus(self, "[TILT]Total steps:" + msg)
-    stepsPerDegreeTilt = 10.55555 # 1/(1.8/19)
-    
-    print " ZEN DONE!", stepsPerDegreePan
-    return (stepsPerDegreePan, stepsPerDegreeTilt)
 
 # Update the status label with given string
 def updateStatus(self, string):
@@ -435,11 +375,11 @@ def processInput(TimeQString,AzQString,ZenQString):
 
 # Take imagePerCamera images from all 3 cameras.  If isCalib is True, 
 # add a string in front of filename to indicate calibration.
-def threeCameraTakeImage(self,az,zen,isCalib,imagePerCamera):
+def takeImages(self,az,zen,isCalib,imagePerCamera):
     updateStatus(self,"Taking images.")
-    # List to hold 6 frames.
+    # List to hold 4 frames.
     frames = []
-    # List to hold filename for 6 frames.
+    # List to hold filename for 4 frames.
     fileNames = []
     
     calibString =""
@@ -463,7 +403,7 @@ def threeCameraTakeImage(self,az,zen,isCalib,imagePerCamera):
     # Read frame from each camera.  Do this imagePerCamera number of times.
     while fps._numFrames < imagePerCamera:
         # Make sure that we are grabbing new frames each time with freshFrame
-        if vs.freshFrame == vs1.freshFrame == vs2.freshFrame == True: # CHANGE HERE IF USING CAMERA 3!!!
+        if vs.freshFrame == vs1.freshFrame == True:#== vs2.freshFrame == True: # CHANGE HERE IF USING CAMERA 3!!!
             # Read a frame and the time of frame.
             frame, imageTime = vs.read()
             # Append frame to frames list.
@@ -475,10 +415,10 @@ def threeCameraTakeImage(self,az,zen,isCalib,imagePerCamera):
             frames.append(frame1)
             fileNames.append("images/" + calibString + imageTime1 + statString + "c2#"+str(fps._numFrames)+".jpg")
              
-            frame2, imageTime2 = vs2.read()
-            frames.append(frame2)
-            fileNames.append("images/" + calibString + imageTime2 + statString + "C3#"+str(fps._numFrames)+".jpg")
-            
+#             frame2, imageTime2 = vs2.read()
+#             frames.append(frame2)
+#             fileNames.append("images/" + calibString + imageTime2 + statString + "C3#"+str(fps._numFrames)+".jpg")
+#             
             fps.update()
     
     t1 = time.time()
@@ -488,11 +428,24 @@ def threeCameraTakeImage(self,az,zen,isCalib,imagePerCamera):
     #print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
     #print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
     
-    for i in range(0,imagePerCamera*3):
+    for i in range(0,imagePerCamera*2):
         cv2.imwrite(fileNames[i],frames[i]) 
     
     updateStatus(self,"Idle.")
-    
+# How to open a pop up, might need later...
+def openPopup(self):
+    print "Opening a new popup window..."
+    self.w = MyPopup()
+    self.w.setGeometry(QRect(100, 100, 400, 200))
+    self.w.show()    
+class MyPopup(QWidget):  
+    def __init__(self):
+        QWidget.__init__(self)
+
+        def paintEvent(self, e):
+            dc = QPainter(self)
+            dc.drawLine(0, 0, 100, 100)
+            dc.drawLine(100, 0, 0, 100) 
 # Get current hour-minutes-seconds.  Used for creating filenames when user saves video.
 def getTime():
     dt = datetime.datetime.now()
@@ -524,12 +477,12 @@ def convertImage(frame):
 def exitFunction():
     vs.stop()
     vs1.stop()
-    vs2.stop()
+    #vs2.stop()
     
     if out != None:
         out.release()
         out1.release()
-        out2.release()
+        #out2.release()
     print "exiting..."
 
 #******************************************************** Main method here ******************************************************************
@@ -540,20 +493,4 @@ app.exec_()                         # and execute the app
 exitFunction()
 sys.exit()
 
-
-
-# How to open a pop up, might need later...
-  #def openPopup(self):
-        #print "Opening a new popup window..."
-        #self.w = MyPopup()
-        #self.w.setGeometry(QRect(100, 100, 400, 200))
-        #self.w.show()    
-#class MyPopup(QWidget):
-    #def __init__(self):
-        #QWidget.__init__(self)
-
-        #def paintEvent(self, e):
-        #dc = QPainter(self)
-        #dc.drawLine(0, 0, 100, 100)
-        #dc.drawLine(100, 0, 0, 100)
               
